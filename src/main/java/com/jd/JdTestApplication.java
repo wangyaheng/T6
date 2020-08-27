e0dc214682e719fffe472c28abb09729bfd4643e
704347630@qq.com <Wyh15010535980>
2020-08-27 13:38:50 +0800

    123

--- src/main/java/com/jd/JdTestApplication.java
+++ src/main/java/com/jd/JdTestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdTestApplication {
+    //asfdasfdasdfa
     public static void main(String[] args) {
         SpringApplication.run(JdTestApplication.class, args);
     }
